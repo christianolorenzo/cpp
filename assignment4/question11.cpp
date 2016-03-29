669862e486afb3bef6d43bccc9975f3b6485c998
christianolorenzo <christiano.lorenzo@gmail.com>
2016-03-29 13:43:48 -0500

    update question 11

--- assignment4/question11.cpp
+++ assignment4/question11.cpp
@@ -1,6 +1,6 @@
 /* Name   : Lorenzo Christopher
  * ID#    : 20151747
- * Purpose: Program to calculate parking charges.
+ * Purpose: Program to calculate parking fare for customers.
  * Date   : 29.03.2016
  */
 
@@ -13,26 +13,27 @@ int main()
 {
     // Variable declaration
     char type;
-    int hourentered;
-    int minentered;
-    int hourleft;
-    int minleft;
+    int hour_in;
+    int minute_in;
+    int hour_out;
+    int minute_out;
     
     // Menu
     cout << "Welcome" << endl;
+    cout << "Please enter the following details" << endl;
     cout << "Enter The Type of Vehicle" << endl;
     cout << " ( C ) --- for Car " << endl;
     cout << " ( B ) --- for Bus " << endl;
     cout << " ( T ) --- for Truck " << endl;
     
     cout << "Enter hour vehicle entered lot (0 - 24):" << endl;
-    cin >> hourentered;
+    cin >> hour_in;
     cout << "Enter minute vehicle entered lot (0 - 60):" << endl;
-    cin >> minentered;
+    cin >> minute_in;
     cout << "Enter hour vehicle left lot (0 - 24):" << endl;
-    cin >> hourleft;
+    cin >> hour_out;
     cout << "Enter minute vehicle left lot (0 - 60):" << endl;
-    cin >> minleft;
+    cin >> minute_out;
 
 
     // Prompt
@@ -41,12 +42,15 @@ int main()
     //Begginging of comparison
     switch(type){
         case 'C':
+        case 'c':
             car
         break;    
         case 'B':
+        case 'b':
             bus
         break;
         case 'T':
+        case 't':
             truck
         break;
         default :
